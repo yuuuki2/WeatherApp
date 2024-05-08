9f14343715295f755679231751345e2010fc2e55
Sascha Zhang <sascha.zhang@student.htl-rankweil.at>
2024-05-08 15:43:12 +0200

    added in OpenWeatherAPI.cs two more classes (Weather and MainData)

--- src/Weatherapp/OpenWeatherAPI.cs
+++ src/Weatherapp/OpenWeatherAPI.cs
@@ -3,12 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Weatherapp
 {
     class OpenWeatherAPI
     {
-        public string CityName { get; set; }
+        public class Weather
+        {
+            int Id { get; set; }
+            string Name { get; set; }
+            string Description { get; set; }
+            string Icon { get; set; }
+
+
+
+        }
+
+        public class MainData
+        {
+            public double TempInKelvin { get; set; }
+
+            public double FeelsLikeInKelvin { get; set; }
+
+            public int Pressure { get; set; }
+
+            public int Humidity { get; set; }
+
+        }
+
+
+
+
+
+
+
 
   
 
